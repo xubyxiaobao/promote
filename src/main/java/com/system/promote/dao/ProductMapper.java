7544ebe06c2fdaecf0fe9da97287a52dabf68f9d
xutao_bxbank <xutao@aibank.com>
2019-08-14 18:08:40 +0800

    项目初始化

---
+++ src/main/java/com/system/promote/dao/ProductMapper.java
@@ -0,0 +1,10 @@
+package com.system.promote.dao;
+
+import com.system.promote.entity.Product;
+
+import java.util.List;
+
+public interface ProductMapper {
+
+    List<Product> queryAll();
+}
---
+++ src/main/java/com/system/promote/entity/Product.java
@@ -0,0 +1,14 @@
+package com.system.promote.entity;
+
+import lombok.Data;
+
+/**
+ * @author xutao
+ * created 2019/8/14 17:57
+ */
+@Data
+public class Product {
+
+
+
+}
