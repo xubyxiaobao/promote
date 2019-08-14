0631abed41a2cf0bd2e3ae0118c8cf84a13c02b1
xutao_bxbank <xutao@aibank.com>
2019-08-14 14:38:04 +0800

    Initial commit

---
+++ src/main/java/com/system/promote/PromoteApplication.java
@@ -0,0 +1,13 @@
+package com.system.promote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromoteApplication.class, args);
+    }
+
+}
